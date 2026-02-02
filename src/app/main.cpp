882f0ff712fc42d4a5eca8950c7f63871695fe42
Demax_games <davydockindima@gmail.com>
2026-02-02 23:29:37 +0300

    new branch

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -14,57 +14,6 @@
 #include <windows.h>
 
 int main(){
-    dem::Logger* logger = dem::Logger::get();
     
-    dem::Mesh* mesh = new dem::Mesh();
-    mesh->Load("examples/cube.obj");
-    
-    dem::Object* object = new dem::Object(mesh);
-    object->position = dem::math::vec3(0.5f, 0.0f, -15.4f);
-    
-    dem::Mesh* doritosMesh = new dem::Mesh();
-    doritosMesh->Load("examples/doritos.obj");
-
-    dem::Object* doritos = new dem::Object(doritosMesh);
-    doritos->position = dem::math::vec3(-2.5f, 0.0f, -0.4f);
-    
-    dem::Camera* cam = new dem::Camera();
-    cam->nearClip = 0.005f;
-    cam->fov = 3.14/1.7f;
-    
-    dem::Scene scene;
-    scene.camera = cam;
-    scene.objects.push_back(object);
-    scene.objects.push_back(doritos);
-    
-    dem::Renderer::Init(1920, 1080);
-    dem::Renderer::LoadScene(scene);
-
-    dem::Input::Init();
-    dem::Input::sensivity = 3.f;
-
-    float deltaTime = 0;
-    int frameCounter = 0;
-    while(!dem::Renderer::Render(scene)){
-        frameCounter++;
-
-        cam->rotation += dem::math::vec3(0.f, 1.5f, 0.f) * dem::Input::GetAxisX() * deltaTime;
-        cam->localRotation += dem::math::vec3(1.5f, 0.f, 0.f) * dem::Input::GetAxisY() * deltaTime;
-
-        if(dem::Input::GetKey(dem::KeyCode::W)) cam->position += cam->forward * 1.5f * deltaTime;
-        if(dem::Input::GetKey(dem::KeyCode::S)) cam->position += cam->forward * -1.5f * deltaTime;
-        if(dem::Input::GetKey(dem::KeyCode::A)) cam->position += cam->right * -1.5f * deltaTime;
-        if(dem::Input::GetKey(dem::KeyCode::D)) cam->position += cam->right * 1.5f * deltaTime;
-
-        object->rotation = object->rotation + dem::math::vec3((float) deltaTime * 0.09f, 0.f, 0.f);
-
-        dem::Input::Update();
-        dem::Time::Update();
-        deltaTime = dem::Time::GetDeltaTime();
-        //std::cout << "\rfps: " << 1.f / deltaTime;
-    }
-
-    logger->log("Total frames generated: ", frameCounter);
-
     return 0;
 }
